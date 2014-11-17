7fb208b29d90b8dda7cf1c78bbfc43e0d3a08d36
Jeffrey van der Stad <jeffrey@hybridsaas.com>
2014-11-17 12:37:35 +0100

    Initial commit

---
+++ src/product-detail/product-detail.js
@@ -0,0 +1,77 @@
+/// <reference path="../definition/jquery.d.ts" />
+
+$(function () {
+    //small to big
+    var $smallImages = $('.smallimage');
+    var $smallImagesFrames = $('.product-image-rest .imageFrame');
+
+    $smallImages.click(function (event) {
+        $smallImagesFrames.removeClass('active');
+
+        var $imagebig = $('#imagebig');
+        var $target = $(event.target);
+        var url = '/image/product/guid/' + encodeURIComponent($imagebig.data('guid')) + '/' + $target.data('index') + '?height=280&width=400';
+
+        $target.parent().addClass('active');
+
+        $imagebig.attr('src', url).hide().fadeIn(250);
+    });
+
+    if (!WebPage.Data.productGuid)
+        return;
+
+    //retreive related products
+    $.getJSON('/data/product/guid/' + encodeURIComponent(WebPage.Data.productGuid) + '/related-products').done(function (data) {
+        //has related
+        if (data.related) {
+            //append to options boxs
+            var $related = $('<div class="related-container"></div>');
+            $('.product-detail').append($related);
+
+            var handler = function (products, title) {
+                if (typeof products != 'undefined') {
+                    var $color = $('<div class="related color"><div class="imageFrame"><div class="label">' + title + '</div><div class="images"></div></div></div>');
+
+                    $color.on('click', function (event) {
+                        event.stopPropagation();
+
+                        //andere dicht
+                        $('.related').removeClass('open');
+
+                        $color.addClass('open');
+
+                        WebPage.References.$body.one('click', function () {
+                            $color.removeClass('open');
+                        });
+                    });
+
+                    var $container = $color.find('.images');
+
+                    for (var x = 0; x < products.length; x++) {
+                        var product = products[x];
+                        var $img = $('<img src="/image/product/guid/' + encodeURIComponent(product.guid) + '?width=135&height=94" />');
+                        $img.attr({ 'title': product.productcode + '\n' + product.description });
+                        $img.data('url', product.url);
+                        $container.append($img);
+                    }
+
+                    $container.find('img').on('click', function (event) {
+                        var $this = $(event.target);
+                        if ($this.closest('.related.open').length) {
+                            location.href = $this.data('url');
+                        }
+                    });
+
+                    $related.append($color);
+                }
+            };
+
+            handler(data.related["Size"], 'Size');
+            handler(data.related["Color"], 'Color');
+        }
+    });
+
+    var $bigImage = $('<div class="big-image"></div>');
+
+    $('.product-right').prepend($bigImage);
+});
---
+++ src/product-detail/product-detail.ts
@@ -0,0 +1,96 @@
+/// <reference path="../definition/jquery.d.ts" />
+declare var WebPage: any;
+
+$(() => {
+
+
+    //small to big
+
+    var $smallImages = $('.smallimage');
+    var $smallImagesFrames = $('.product-image-rest .imageFrame');
+
+    $smallImages.click((event) => {
+
+        $smallImagesFrames.removeClass('active');
+
+        var $imagebig = $('#imagebig');
+        var $target = $(event.target);
+        var url = '/image/product/guid/' + encodeURIComponent($imagebig.data('guid')) + '/' + $target.data('index') + '?height=280&width=400';
+
+        $target.parent().addClass('active');
+
+
+        $imagebig.attr('src', url).hide().fadeIn(250);
+
+    });
+
+    if (!WebPage.Data.productGuid)
+        return;
+
+    //retreive related products
+    $.getJSON('/data/product/guid/' + encodeURIComponent(WebPage.Data.productGuid) + '/related-products')
+        .done((data) => {
+
+            //has related
+            if (data.related) {
+
+                //append to options boxs
+                var $related = $('<div class="related-container"></div>');
+                $('.product-detail').append($related);
+
+
+                var handler = function(products, title) {
+
+                    if (typeof products != 'undefined') {
+
+                        var $color = $('<div class="related color"><div class="imageFrame"><div class="label">' + title + '</div><div class="images"></div></div></div>');
+
+                        $color.on('click', (event: JQueryEventObject) => {
+                            event.stopPropagation();
+
+                            //andere dicht  
+                            $('.related').removeClass('open');
+
+                            $color.addClass('open');
+
+                            WebPage.References.$body.one('click', () => {
+                                $color.removeClass('open');
+                            });
+                        });
+
+                        var $container = $color.find('.images');
+
+                        for (var x = 0; x < products.length; x++) {
+                            var product = products[x];
+                            var $img = $('<img src="/image/product/guid/' + encodeURIComponent(product.guid) + '?width=135&height=94" />');
+                            $img.attr({ 'title': product.productcode + '\n' + product.description });
+                            $img.data('url', product.url);
+                            $container.append($img);
+                        }
+
+                        $container.find('img').on('click', (event: JQueryEventObject) => {
+                            var $this = $(event.target);
+                            if ($this.closest('.related.open').length) {
+                                
+                                location.href = $this.data('url');
+
+                            }
+                        });
+
+
+                        $related.append($color)
+                    }
+                };
+
+                handler(data.related["Size"], 'Size');
+                handler(data.related["Color"], 'Color');
+            }
+        });
+
+
+    var $bigImage = $('<div class="big-image"></div>');
+
+    $('.product-right').prepend($bigImage);
+
+
+})
