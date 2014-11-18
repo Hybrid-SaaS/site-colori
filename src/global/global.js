9592bae8fdadd35e86aa1669e44d37d00beec8a8
Rick Littel <rick@hybridsaas.com>
2014-11-18 11:43:48 +0100

    Add to Basket knop toegevoegd

--- src/global/global.js
+++ src/global/global.js
@@ -1,4 +1,5 @@
 /// <reference path="../definition/jquery.d.ts" />
+
 $(function () {
     //onload
     var $menuItems = $('.main-column-left nav > ul > li');
@@ -9,5 +10,21 @@ $(function () {
         if ($(elem).find('.hybridsaas-menu-current').length != 0) {
             $(elem).addClass('show-menu');
         }
+    });
+
+    var $products = $('.container > .product');
+    var buyButton = $(document.createElement('div'));
+    $products.each(function (index, elem) {
+        $('<input/>', {
+            'type': 'button',
+            'value': 'Add to basket',
+            'class': 'buyButton',
+            'click': function () {
+                $.getJSON('/Website/Basket/Add', { 'product': $(elem).data('productId'), 'amount': 1 }).done(function () {
+                    return WebPage.Basket.updateClient();
+                });
+                return false;
+            }
+        }).appendTo($(elem).children('.price'));
     });
 });
--- src/global/global.ts
+++ src/global/global.ts
@@ -1,4 +1,5 @@
 /// <reference path="../definition/jquery.d.ts" />
+declare var WebPage: any;
 
 $(() => {
 
@@ -9,5 +10,19 @@ $(() => {
         if ($(elem).find('.hybridsaas-menu-current').length != 0) {
             $(elem).addClass('show-menu');
         }
+    });
+
+    var $products = $('.container > .product');
+    var buyButton = $(document.createElement('div'));
+    $products.each((index, elem) => {
+        $('<input/>', {
+            'type': 'button',
+            'value': 'Add to basket',
+            'class': 'buyButton',
+            'click': function () {
+                $.getJSON('/Website/Basket/Add', { 'product': $(elem).data('productId'), 'amount': 1 }).done(() => WebPage.Basket.updateClient());
+                return false;
+            }
+        }).appendTo($(elem).children('.price'));
     });
 });
