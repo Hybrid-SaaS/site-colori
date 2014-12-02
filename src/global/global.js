d1feda777696aae4c2d3574765d62f640668408a
Rick Littel <rick@hybridsaas.com>
2014-12-02 17:29:36 +0100

    Styling footer, menu status bewaren

--- src/global/global.js
+++ src/global/global.js
@@ -281,12 +281,20 @@ $(function () {
 $(function () {
     //onload
     var $menuItems = $('.main-column-left nav>ul>li>span');
+    var index = getCookie('indexmenu');
+    if (index != '') {
+        var parsedindex = parseInt(index);
+        $menuItems.eq(parsedindex).parent().addClass('show-menu');
+    } else {
+        $menuItems.eq(0).parent().addClass('show-menu');
+    }
     $menuItems.click(function (event) {
-        $(event.delegateTarget).parent().toggleClass('show-menu');
-    });
-    $menuItems.each(function (index, elem) {
-        if ($(elem).find('.hybridsaas-menu-current').length != 0) {
-            $(elem).addClass('show-menu');
+        var c = $(event.delegateTarget).parent();
+        c.toggleClass('show-menu');
+        if (c.hasClass('show-menu')) {
+            document.cookie = "indexmenu=" + c.index();
+        } else {
+            document.cookie = "indexmenu=0";
         }
     });
 
@@ -393,3 +401,16 @@ $(function () {
     }
     ;
 });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ')
+            c = c.substring(1);
+        if (c.indexOf(name) != -1)
+            return c.substring(name.length, c.length);
+    }
+    return "";
+}
--- src/global/global.ts
+++ src/global/global.ts
@@ -266,15 +266,23 @@ $(() => WebPage.load());
 $(() => {
 
     //onload
-    var $menuItems = $('.main-column-left nav>ul>li>span');
-    $menuItems.click((event) => {
-        $(event.delegateTarget).parent().toggleClass('show-menu');
-    });
-    $menuItems.each((index, elem) => {
-        if ($(elem).find('.hybridsaas-menu-current').length != 0) {
-            $(elem).addClass('show-menu');
-        }
-    });
+	var $menuItems = $('.main-column-left nav>ul>li>span');
+	var index = getCookie('indexmenu');
+	if (index != '') {
+		var parsedindex = parseInt(index);
+		$menuItems.eq(parsedindex).parent().addClass('show-menu');
+	} else {
+		$menuItems.eq(0).parent().addClass('show-menu')
+	}
+	$menuItems.click((event) => {
+		var c = $(event.delegateTarget).parent(); 
+		c.toggleClass('show-menu');
+		if (c.hasClass('show-menu')) {
+			document.cookie = "indexmenu=" + c.index();
+		} else {
+			document.cookie = "indexmenu=0";
+		}
+	});
 
     var $products = $('.container > .product');
     var buyButton = $(document.createElement('div'));
@@ -381,3 +389,14 @@ $(() => {
 
 
 });
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1);
+		if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+	}
+	return "";
+} 
